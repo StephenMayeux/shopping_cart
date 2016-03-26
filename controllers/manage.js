7f565563de71fc8396e8717877da30de9415c45c
Stephen Mayeux <stephenmayeux@gmail.com>
2016-03-27 02:44:22 +0900

    add CRUD functionality for books

--- controllers/manage.js
+++ controllers/manage.js
@@ -25,4 +25,52 @@ module.exports = function (router) {
       });
     });
 
+    router.get('/books/add', function(req, res) {
+      Category.find({}, function(err, categories) {
+        if (err) throw err;
+        var model = {categories: categories};
+        res.render('manage/books/add', model);
+      });
+    });
+
+    router.post('/books', function(req, res) {
+      var title     =  req.body.title && req.body.title.trim(),
+          category  =  req.body.category && req.body.category.trim(),
+          author    =  req.body.author && req.body.author.trim(),
+          publisher =  req.body.publisher && req.body.publisher.trim(),
+          price     =  req.body.price && req.body.publisher.trim(),
+          desc      =  req.body.description && req.body.description.trim(),
+          cover     = req.body.cover && req.body.cover.trim();
+
+      /*if (title === '' || price === '') {
+        req.flash('error', 'Please fill out required fields');
+        res.location('/manage/books/add');
+        res.redirect('/manage/books/add');
+      }
+
+      if (isNaN(price)) {
+        req.flash('error', 'Price must be a number');
+        res.location('/manage/books/add');
+        res.redirect('/manage/books/add');
+      }*/
+
+      var newBook = new Book({
+        title: title,
+        category: category,
+        description: desc,
+        author: author,
+        publisher: publisher,
+        cover: cover,
+        price: price
+      });
+
+      newBook.save(function(err) {
+        if (err) throw err;
+        req.flash('success', 'Book added!');
+        res.location('/manage/books');
+        res.redirect('/manage/books');
+      });
+
+    });
+
 };
